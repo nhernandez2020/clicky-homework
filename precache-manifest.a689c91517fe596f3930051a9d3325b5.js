6c45f3cbd9f6e1e1efb399503996e4e9187af5d3
Noe Hernandez <Nhernandez2020@hotmail.com>
2020-05-20 01:17:28 -0700

    Updates

---
+++ precache-manifest.a689c91517fe596f3930051a9d3325b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94c329bb07422515adb5535bdac3653",
+    "url": "/clicky-homework/index.html"
+  },
+  {
+    "revision": "aeaf0c5840a17b9c64af",
+    "url": "/clicky-homework/static/css/main.1675bfcb.chunk.css"
+  },
+  {
+    "revision": "43cf0c37321d1f333736",
+    "url": "/clicky-homework/static/js/2.7094c867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-homework/static/js/2.7094c867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeaf0c5840a17b9c64af",
+    "url": "/clicky-homework/static/js/main.cca52428.chunk.js"
+  },
+  {
+    "revision": "7a070ebd7fe486044aaf",
+    "url": "/clicky-homework/static/js/runtime-main.369fb3b0.js"
+  }
+]);
